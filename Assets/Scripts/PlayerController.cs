0395588b64f310f3a2423c88e9986823796deacd
Xd0uc3tt3 <145155890+Xd0uc3tt3@users.noreply.github.com>
2025-09-28 16:19:06 -0300

    Fixed Movement
    Do you know the definition of insanity, insanity is doing the exact.. same thing... over and over again expecting.. something to change

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,23 +9,25 @@ public class PlayerController : MonoBehaviour
     private float movementX;
     private float movementY;
 
-    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    private void FixedUpdate()
-    {
-        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
-        rb.AddForce(movement);
-    }
-
     void OnMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
+
         movementX = movementVector.x;
         movementY = movementVector.y;
     }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        rb.AddForce(movement);
+    }
+
 }
 
