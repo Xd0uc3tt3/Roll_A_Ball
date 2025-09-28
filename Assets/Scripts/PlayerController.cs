a84da99a3af6ec7e0c47d9153162228d94baa35c
Xd0uc3tt3 <145155890+Xd0uc3tt3@users.noreply.github.com>
2025-09-28 17:29:11 -0300

    Added PickUp Collision

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -30,5 +30,15 @@ public class PlayerController : MonoBehaviour
         rb.AddForce(movement * speed);
     }
 
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+        }
+        
+
+    }
+
 }
 
