679aabf1a388f2bd7d7500a0bbd770b05cf35699
Xd0uc3tt3 <145155890+Xd0uc3tt3@users.noreply.github.com>
2025-10-21 19:26:18 -0300

    Added start game and quit game functionality

---
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Lobby");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+}
+
