a0d953ff1af6e86b578e42c41da278c93b4d0952
Xd0uc3tt3 <145155890+Xd0uc3tt3@users.noreply.github.com>
2025-11-03 13:20:34 -0400

    Added Background theme to Jump Rope

---
+++ Assets/Scripts/JumpRope/JumpRopeBackground.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class JumpRopeMusicPlayer : MonoBehaviour
+{
+    private static JumpRopeMusicPlayer instance;
+    private AudioSource audioSource;
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "Lobby")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
